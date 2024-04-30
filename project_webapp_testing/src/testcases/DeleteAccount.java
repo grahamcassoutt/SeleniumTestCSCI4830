2c1723158e03198161c7c600cb8d5c17d50876ee
grahamcassoutt <grahamc@jeveg.com>
2024-04-30 15:55:42 -0500

    Intial

---
+++ project_webapp_testing/src/testcases/DeleteAccount.java
@@ -0,0 +1,104 @@
+package testcases;
+
+import static org.junit.Assert.fail;
+
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DeleteAccount {
+   private WebDriver driver;
+   private String baseUrl;
+   private boolean acceptNextAlert = true;
+   private StringBuffer verificationErrors = new StringBuffer();
+   JavascriptExecutor js;
+
+   @Before
+   public void setUp() throws Exception {
+      System.setProperty("webdriver.chrome.driver", "lib/mac/chromedriver");
+      driver = new ChromeDriver();
+      baseUrl = "http://127.0.0.1:8000/";
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+      js = (JavascriptExecutor) driver;
+   }
+
+   @Test
+   public void testUntitledTestCase() throws Exception {
+      driver.manage().window().maximize();
+      driver.get(baseUrl);
+      driver.findElement(By.linkText("Account")).click();
+      Thread.sleep(1500);
+      driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul/li[2]/div/a[1]")).click();
+      Thread.sleep(1500);
+      driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("Test");
+      driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("bIll#123");
+      Thread.sleep(1500);
+      driver.findElement(By.className("btn-primary")).click();
+      Thread.sleep(1500);
+      driver.findElement(By.linkText("Account")).click();
+      Thread.sleep(1500);
+      driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul/li[3]/div/a[1]")).click();
+      Thread.sleep(1500);
+      driver.findElement(By.className("delete-account-btn")).click();
+      Thread.sleep(2000);
+      Assert.assertEquals("Your account has been successfully deleted.", driver.findElement(By.className("alert-danger")).getText());
+      Thread.sleep(3000);
+   }
+
+   @After
+   public void tearDown() throws Exception {
+      driver.quit();
+      String verificationErrorString = verificationErrors.toString();
+      if (!"".equals(verificationErrorString)) {
+         fail(verificationErrorString);
+      }
+   }
+
+   @SuppressWarnings("unused")
+   private boolean isElementPresent(By by) {
+      try {
+         driver.findElement(by);
+         return true;
+      } catch (NoSuchElementException e) {
+         return false;
+      }
+   }
+
+   @SuppressWarnings("unused")
+   private boolean isAlertPresent() {
+      try {
+         driver.switchTo().alert();
+         return true;
+      } catch (NoAlertPresentException e) {
+         return false;
+      }
+   }
+
+   @SuppressWarnings("unused")
+   private String closeAlertAndGetItsText() {
+      try {
+         Alert alert = driver.switchTo().alert();
+         String alertText = alert.getText();
+         if (acceptNextAlert) {
+            alert.accept();
+         }
+         else {
+            alert.dismiss();
+         }
+         return alertText;
+      } finally {
+         acceptNextAlert = true;
+      }
+   }
+
+}
